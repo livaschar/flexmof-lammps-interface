c4bac29e4c568fad3bf68a81477e22dad41127fc
Charalampos Livas <livas.charalampos@gmail.com>
2025-03-20 18:13:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="lammps-interface",
-    author="Peter Boyd, Mohamad Moosavi, Matthew Witman",
+    name="flexmof-lammps-interface",
+    author="Charalampos G. Livas",
     python_requires=">=3.6",
     classifiers=[
         "License :: OSI Approved :: MIT License",
@@ -21,18 +21,18 @@ setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     license="MIT",
-    url="https://github.com/peteboyd/lammps_interface",
-    description="Automatic generation of LAMMPS input files for molecular dynamics simulations of MOFs",
+    url="https://github.com/livaschar/flexmof-lammps-interface",  # Update the URL to your repo
+    description="Automatic generation of LAMMPS input files for molecular dynamics simulations of MOFs.",
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     install_requires=requirements,
     extras_require={
-        'docs': [ 'sphinx', 'sphinx-rtd-theme' ],
-        'tests': [ 'pytest' ]
+        'docs': ['sphinx', 'sphinx-rtd-theme'],
+        'tests': ['pytest'],
     },
     entry_points={
         'console_scripts': [
-            'lammps-interface = lammps_interface.cli:main'
+            'flexmof-lammps-interface = flexmof_lammps_interface.cli:main',  # Update the entry point
         ]
     },
     include_package_data=True,
